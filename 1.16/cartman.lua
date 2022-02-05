d0b1dcccdf28fffab59c384624457864d95db427
Luke Turner <github@luketurner.org>
2022-02-04 19:08:52 -0800

    add WIP cartman program

---
+++ 1.16/cartman.lua
@@ -0,0 +1,58 @@
+-- Minecart loader/unloader manager program
+-- Copyright 2022 Luke Turner. Released under the MIT License.
+-- Tested with Create Above and Beyond modpack, your mileage may vary!
+
+print("")
+print("= CARTMAN =")
+print("")
+
+local inventories = { peripheral.find("inventory") }
+
+local loaders = {}
+local unloaders = {}
+local otherInventories = {}
+
+for _, x in ipairs(inventories) do
+    if peripheral.hasType(x, "moreminecarts:minecart_unloader_te") then
+        table.insert(unloaders, x)
+    elseif peripheral.hasType(x, "moreminecarts:minecart_loader_te") then
+        table.insert(loaders, x)
+    else
+        table.insert(otherInventories, x)
+    end
+end
+
+print("Loaders:")
+for _, x in ipairs(loaders) do print("  ", peripheral.getName(x)) end
+
+print("Unloaders:")
+for _, x in ipairs(unloaders) do print("  ", peripheral.getName(x)) end
+
+print("Inventories:")
+for _, x in ipairs(otherInventories) do print("  ", peripheral.getName(x)) end
+
+function pushBalanced(srcInv, destInvs)
+    for slot, item in pairs(srcInv.list()) do
+        local itemCount = item.count
+        local numInvs = #destInvs
+        for _, inv in ipairs(destInvs) do
+            local invName = peripheral.getName(inv)
+            local itemsPerInv = math.floor((itemCount - 1) / numInvs)
+            local itemsMoved = unloader.pushItems(invName, slot, itemsPerInv)
+            print(("\nmoved %d %s to %s"):format(itemsMoved, item.name, invName))
+            itemCount = itemCount - itemsMoved
+            numInvs = numInvs - 1
+        end
+    end
+end
+
+while true do
+    term.write(".")
+    for _, unloader in ipairs(unloaders) do
+        pushBalanced(unloader, otherInventories)
+    end
+    for _, inv in ipairs(otherInventories) do
+        pushBalanced(inv, loaders)
+    end
+    sleep(10)
+end
